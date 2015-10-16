179e98190fb98cc40262f8073ef6f03f6b6e4d81
Brett Lyons <blyons@students.naropa.edu>
2015-10-16 14:21:21 -0600

    server.js up and running returns tasks from db

--- server.js
+++ server.js
@@ -1,37 +1,23 @@
 var http = require('http');
-var url = require('url');
-var monk = require('monk');
+// var url = require('url');
+// require('dotenv').load()
+//var db = require('monk')(process.env.MONGOLAB_URI);
+var db = require('monk')('localhost/tasksdb');
+var tasks = db.get('tasks');
 
 var server = http.createServer(function (req, res) {
   if(req.method != 'GET'){
     return res.end('Send me a GET\n');
   }
-  res.writeHead(200, {'Content-Type':'application/json'});
-  var urlKeys = url.parse(req.url, true);
-  // so I want the pathname of /api/parsetime
-  // and query of {iso: 'string to be split'}
-  //console.log(urlKeys.query.iso);
-  // and also to respond differently to /api/unixtime
-  var jsonResponse = {};
-
-  if (urlKeys.pathname == '/api/parsetime') {
-    var tmpDate = new Date(urlKeys.query.iso);
-      //console.log(tmpDate)
-    jsonResponse.hour = tmpDate.getHours();
-    jsonResponse.minute = tmpDate.getMinutes();
-    jsonResponse.second = tmpDate.getSeconds();
-      //console.log(jsonResponse);
-    res.end(JSON.stringify(jsonResponse));
-  }
-  if (urlKeys.pathname == '/api/unixtime') {
-    var tmpDate = new Date(urlKeys.query.iso);
-    jsonResponse.unixtime = tmpDate.getTime();
-    res.end(JSON.stringify(jsonResponse));
-  }
-  else {
-    res.writeHead(404);
-    res.end("Your request is unknown to the server.");
-  }
-})
+  tasks.find({}, function(err, tasks) {
+    res.writeHead(200, {'Content-Type':'application/json'});
+    res.end(JSON.stringify(tasks)); // tasks is an array here.
+  }); // without the POST boilerplate, this is 20 lines
+  // if(req.method == "POST") {
+  //   tasks.insert({
+  //     //put new task description into db . . .
+  //   });
+  // }
+});
 
 server.listen(Number(process.argv[2]));
