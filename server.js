c75008f0c8e628d86f1d60302f22073e317a2c88
Brett Lyons <blyons@students.naropa.edu>
2015-10-16 15:18:21 -0600

    Set up server to serve index.html as wel

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 var http = require('http');
-// var url = require('url');
+var url = require('url');
+var staticServer = require('node-static');
 // require('dotenv').load()
 //var db = require('monk')(process.env.MONGOLAB_URI);
 var db = require('monk')('localhost/tasksdb');
 var tasks = db.get('tasks');
 
+var file = new(staticServer.Server)();
+
 var server = http.createServer(function (req, res) {
+  var urlKeys = url.parse(req.url, true);
   if(req.method != 'GET'){
     return res.end('Send me a GET\n');
   }
-  tasks.find({}, function(err, tasks) {
-    res.writeHead(200, {'Content-Type':'application/json'});
-    res.end(JSON.stringify(tasks)); // tasks is an array here.
-  }); // without the POST boilerplate, this is 20 lines
+  if (urlKeys.pathname == '/api/tasks'){
+    tasks.find({}, function(err, tasks) {
+      res.writeHead(200, {'Content-Type':'application/json'});
+      res.end(JSON.stringify(tasks)); // tasks is an array here.
+    });
+  }
+  else {
+    file.serve(req, res);
+  }
   // if(req.method == "POST") {
   //   tasks.insert({
   //     //put new task description into db . . .
